8f1589e6bbe3eb6cbd9b53211066d9de07baa4a4
aperezmm <alejandro.perez1@udea.edu.co>
2020-08-10 23:20:49 -0500

    Customizing view, and use of the directives angular

--- src/app/models/travel-destination.module.ts
+++ src/app/models/travel-destination.module.ts
@@ -2,10 +2,12 @@ export class TravelDestinationModule {
     name:string;
     url:string;
     private selected: boolean;
+    public services: string[];
 
     constructor(public n:string, public u:string){
         this.name = n;
         this.url = u;
+        this.services = ['Turco', 'Sauna', 'Jacuzzi'];
     }
 
     //BOTON IR
