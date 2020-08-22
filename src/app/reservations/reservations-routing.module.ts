506e7fabcb6b5e92ff83ebe2b3d53df15a98a0a5
aperezmm <alejandro.perez1@udea.edu.co>
2020-08-21 20:40:59 -0500

    Reservation module, component reservations, routes children

--- src/app/reservations/reservations-routing.module.ts
+++ src/app/reservations/reservations-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReservationDetailComponent } from './reservation-detail/reservation-detail.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
 
-const routes: Routes = [];
+//Archivo creado con el --flat --routing
+
+//Utiliza sus propias rutas que son reservations y reservations/:id
+const routes: Routes = [
+  { path: 'reservations', component: ReservationListComponent },
+  { path: 'reservations/:id', component: ReservationDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
